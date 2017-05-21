cdc4ec38b21cf48e7b9259d0a3776675a364c4e5
JayChenFE <chenjiehwy@163.com>
2017-05-21 21:50:28 +0800

    表单验证

--- hello-angular/src/app/login/login.component.ts
+++ hello-angular/src/app/login/login.component.ts
@@ -5,23 +5,34 @@ import { Component, OnInit, Inject } from '@angular/core';
   selector: 'app-login',
   template: `
     <div>
-      <input [(ngModel)]="username" type="text">
-      <input [(ngModel)]="password" type="text">
-      <button (click)="onClick()">Login</button>
+    <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
+      <fieldset ngModelGroup="login">
+      <input required name="username" [(ngModel)]="username" #usernameRef="ngModel" minlength="3" type="text">
+      {{usernameRef.errors|json}}    {{usernameRef.valid}}
+      <span *ngIf="usernameRef.errors?.required">必填</span>
+      <span *ngIf="usernameRef.errors?.minlength">至少3个字符</span>
+      <input  required name="password"  [(ngModel)]="password" #passwordRef="ngModel" type="text">
+     <span *ngIf="passwordRef.errors?.required">必填</span>
+      <button type="submit">Login</button>
+      </fieldset>
+    </form>
     </div>
   `,
   styles: []
 })
 export class LoginComponent implements OnInit {
 
-  username=""
-  password=""
+  username = ""
+  password = ""
   text = "login componet"
-  onClick() {
-    alert(`username:${this.username}\n\rpassword:${this.password}`);
-    alert(`auth result is:  ${this.service.loginWithCredentials(this.username,this.password)}`);
-  }
+  // onClick() {
+  //   alert(`username:${this.username}\n\rpassword:${this.password}`);
+  //   alert(`auth result is:  ${this.service.loginWithCredentials(this.username, this.password)}`);
+  // }
+  onSubmit(formValue) {
+    console.log(`auth result is:${this.service.loginWithCredentials(formValue.login.username, formValue.login.password)}`)
 
+  }
   constructor( @Inject('auth') private service) {
 
   }
