a406855e27d48fe695df7b0935fbca65ac4629eb
JayChenFE <chenjiehwy@163.com>
2017-05-21 22:50:39 +0800

    更改组件样式

--- hello-angular/src/app/login/login.component.ts
+++ hello-angular/src/app/login/login.component.ts
@@ -1,34 +1,61 @@
 import { Component, OnInit, Inject } from '@angular/core';
 
 
+/**
+ *
+ *
+ * @export
+ * @class LoginComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-login',
   template: `
     <div>
     <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
       <fieldset ngModelGroup="login">
-      <input required name="username" [(ngModel)]="username" #usernameRef="ngModel" minlength="3" type="text">
+      <input required name="username" [(ngModel)]="username" #usernameRef="ngModel" minlength="3" type="text" [placeholder]="holder" (focus)="onfocus()">
       {{usernameRef.errors|json}}    {{usernameRef.valid}}
-      <span *ngIf="usernameRef.errors?.required">必填</span>
-      <span *ngIf="usernameRef.errors?.minlength">至少3个字符</span>
+      <div *ngIf="usernameRef.errors?.required">必填</div>
+      <div *ngIf="usernameRef.errors?.minlength">至少3个字符</div>
       <input  required name="password"  [(ngModel)]="password" #passwordRef="ngModel" type="text">
-     <span *ngIf="passwordRef.errors?.required">必填</span>
+      <div *ngIf="passwordRef.errors?.required">必填</div>
       <button type="submit">Login</button>
       </fieldset>
     </form>
     </div>
   `,
-  styles: []
+  styles: [`
+  input.ng-invalid{
+    border:3px solid red;
+  }
+  input.ng-valid{
+    border:3px solid green
+  }
+  input::-webkit-input-placeholder
+  {
+    color:red;
+    text-align: right;
+  }
+  input::-ms-input-placeholder{
+    color:red;
+text-align: right;
+  }`]
 })
 export class LoginComponent implements OnInit {
 
+
   username = ""
   password = ""
+  holder = "请输入用户名1"
   text = "login componet"
   // onClick() {
   //   alert(`username:${this.username}\n\rpassword:${this.password}`);
   //   alert(`auth result is:  ${this.service.loginWithCredentials(this.username, this.password)}`);
   // }
+  onfocus() {
+    this.holder = '';
+  }
   onSubmit(formValue) {
     console.log(`auth result is:${this.service.loginWithCredentials(formValue.login.username, formValue.login.password)}`)
 
